2d2275754396c3560095003f144ef2476247169f
mathemecom <matheme.com@gmail.com>
2016-03-01 18:35:54 +0200

    lightbot_haan_jmod_v010d_20151009(many_mazes ver)
    lightbot_haan_jmod_v010d_20151009(create_many_mazes version).zip Taken from mochahost

---
+++ lightbot_haan/map_create_v006ok_but_all_at_start.php
@@ -0,0 +1,89 @@
+<?php
+$counter=0;
+$counter_steps=0;
+$max_lights=5;
+$max_steps=4;
+$debug=0;
+$light_array= array("b", "l");
+$steps_array= array(0, 1);
+if($debug)print_r($light_array);
+
+function getLight(){
+
+	global $counter,$max_lights,$light_array,$debug;
+	//$input = array("b", "l");
+
+	$random=rand(0,1);
+
+	$ret=$light_array[$random];
+
+	if($ret=="l") $counter++;
+	if($debug) echo "<h1>$counter :light:$ret ,random=$random</h1>";
+	if ($counter>$max_lights) $ret="b";
+	//getLight()
+	return $ret;
+}//function getLight(){ ///////////////////////////////
+
+function getStep(){
+
+	global $counter_steps,$max_steps,$steps_array,$debug;
+	//$input = array("b", "l");
+
+	$random=rand(0,1);
+
+	$ret=$steps_array[$random];
+
+	if($ret=="l") $counter_steps++;
+	if($debug) echo "<h1>$counter_steps :light:$ret ,random=$random</h1>";
+	if ($counter_steps>$max_steps) $ret="0";
+	//getLight()
+	return $ret;
+}//function getStep(){ ////////////////////////////////////////////////////////////////
+
+
+echo '
+{"direction":0,"position": {"x": 4, "y": 5},
+"map":
+[[
+
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"}
+],[
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"}
+],[
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"}
+],[
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"}
+],[
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"}
+],[
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"},
+{"h":'.getStep().', "t":"'.getLight().'"}
+
+]],"medals":{"gold":5,"silver":10,"bronze":25}}
+
+';
+
+?>  
---
+++ lightbot_haan/select_old.php
@@ -0,0 +1,23 @@
+<a href=index.php?map=maps.txt >maps </a><br>
+<a href=index.php?map=maps_nax.txt >maps_nax </a><br>
+<a href=index.php?map=maps_easy.txt >maps_easy </a><br>
+<a href=index.php?map=maps_d1.txt >maps_d1 (numbers) </a><br>
+<a href=index.php?map=maps.txt >maps </a><br>
+<a href=index.php?map=maps_temp.txt >maps_temp </a><br>
+<a href=index.php?map=maps_random_2(5lights).txt >maps_random_2(5lights) </a><br>
+<a href=index.php?map=maps_random_3(5lights4x4).txt >maps_random_3(5lights4x4) </a><br>
+
+maps_random_3(5lights4x4).txt
+<hr>
+
+
+
+<a href=maps_easy.php >maps_easy.php </a><br>
+<a href=maps_nax.php >maps_nax.php </a><br>
+<a href=maps_orig.php >maps_orig.php </a><br>
+<a href=maps_easy.php >maps_easy.php </a><br>
+<a href=maps_easy.php >maps_easy.php </a><br>
+<?php
+
+
+?>
