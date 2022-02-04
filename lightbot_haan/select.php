e6d9541be73c4b925c8756adbc8ced8b07f00159
aaa <aaa@aaa.com>
2022-02-04 20:25:53 +0200

    220103 - renamed maps -removed parenthesis

--- lightbot_haan/select.php
+++ lightbot_haan/select.php
@@ -1,5 +1,20 @@
 
-<h2> </h2>
+<h2>Please Select - Map </h2>
+
+<hr>
+<a href=index.html?map=maps.txt >maps </a><br>
+<a href=index.html?map=maps_nax.txt >maps_nax </a><br>
+<a href=index.html?map=maps_easy.txt >maps_easy </a><br>
+<a href=index.html?map=maps_d1.txt >maps_d1 (numbers) </a><br>
+<a href=index.html?map=maps.txt >maps </a><br>
+<a href=index.html?map=maps_temp.txt >maps_temp </a><br>
+<a href=index.html?map=maps_random_1__4x4_3lights.txt >maps_random_1__4x4_3lights.txt </a><br>
+<a href=index.html?map=maps_random_2__5lights.txt >maps_random_2(5lights) </a><br>
+<a href=index.html?map=maps_random_3__5lights.txt >maps_random_3(5lights) </a><br>
+<a href=index.html?map=maps_loops_only1.txt >maps_loops_only1.txt </a><br>
+
+<hr>
+
 
 <hr>
 <a href=index.php?map=maps.txt >maps </a><br>
@@ -8,8 +23,9 @@
 <a href=index.php?map=maps_d1.txt >maps_d1 (numbers) </a><br>
 <a href=index.php?map=maps.txt >maps </a><br>
 <a href=index.php?map=maps_temp.txt >maps_temp </a><br>
-<a href=index.php?map=maps_random_2(5lights).txt >maps_random_2(5lights) </a><br>
-<a href=index.php?map=maps_random_3(5lights).txt >maps_random_3(5lights) </a><br>
+<a href=index.php?map=maps_random_1__4x4_3lights.txt >maps_random_1__4x4_3lights.txt </a><br>
+<a href=index.php?map=maps_random_2__5lights.txt >maps_random_2(5lights) </a><br>
+<a href=index.php?map=maps_random_3__5lights.txt >maps_random_3(5lights) </a><br>
 <a href=index.php?map=maps_loops_only1.txt >maps_loops_only1.txt </a><br>
 
 <hr>
